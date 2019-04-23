c02cc71001577a237c92efa0aa369926a2d3cb63
Alexander Shachor <alexshachor@gmail.com>
2019-04-23 06:56:45 +0300

    change sttings view

--- FlightSimulator/Views/Settings.xaml.cs
+++ FlightSimulator/Views/Settings.xaml.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using FlightSimulator.Model;
+using FlightSimulator.ViewModels.Windows;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -22,6 +24,7 @@ namespace FlightSimulator.Views
         public Settings()
         {
             InitializeComponent();
+            DataContext = new SettingsWindowViewModel(new ApplicationSettingsModel());
         }
     }
 }
